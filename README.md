364dd10e2c17b27d098d51b49a190cceb0d0b013
clindsey2 <clindsey2@liberty.edu>
2021-11-10 01:56:59 -0500

    writing files

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ from: freeCodeCamp.org
 
 published: July 11, 2018
 
-i stopped at 3:21:27  of 4:26:51
+i stopped at 3:28:27  of 4:26:51
 
 https://www.youtube.com/watch?v=rfscVS0vtbw
--- writing_to_files.py
+++ writing_to_files.py
@@ -1,4 +1,10 @@
 
-'''
-start here to resume learning
-'''
+# #employee_file = open("employees.txt", "a")
+# employee_file = open("employees1.txt", "w")
+# employee_file.write("\nToby - Human Resources")
+# employee_file.write("\nKelly - Customer Service")
+
+employee_file = open("index.html", "w")
+employee_file.write("\n<p>This is HTML</p>")
+
+employee_file.close()
