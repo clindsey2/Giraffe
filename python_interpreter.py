b2c4f5b704c9827494e2156b6c4cefda9d5b2f1b
clindsey2 <clindsey2@liberty.edu>
2021-11-11 05:05:34 -0500

    help on the python interpreter

---
+++ python_interpreter.py
@@ -0,0 +1,19 @@
+
+# study help information on using the python interpreter
+#
+#  use the windows command prompt or the Terminal below in intelleJ
+#
+#    type python3 to get into the interpreter
+#     >>> print ("Hello World")
+#
+#     >>> num1 = 10
+#     >>> num2 = 90
+#     >>> print(num1 + num2)
+#    100
+#
+#     >>> def say_hi(name):
+#     ...   print("Hello " + name);
+#     >>> say_hi("Mike")
+#
+#
+
